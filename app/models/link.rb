d965b771b417bb98972d5cad8a052383efcf9c2a
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-05 19:33:40 -0600

    add api functionality

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,17 @@
 class Link < ApplicationRecord
   validates :url, presence: true
+  after_save :hottest_read
 
   def self.top_ten
     time_range = (Time.now - 1.day)..Time.now
     self.where(:updated_at => time_range)
     .order("count DESC")
     .limit(10)
+  end
+
+  def hottest_read
+    if self == Link.top_ten.first
+      self.hot = true
+    end
   end
 end
--- spec/requests/api/v1/links_request_spec.rb
+++ spec/requests/api/v1/links_request_spec.rb
@@ -12,17 +12,4 @@ describe "links API" do
     expect(link.url).to eq(params[:url])
   end
 
-  it "can edit a link" do
-    link    = create(:link, url: "http://www.google.com")
-    link_id = link.id
-    params  = { url: "http://www.google.com", count: 2 }
-
-    patch "/api/v1/links/#{link_id}",  params: params
-    link = Link.find_by(url: "http://www.google.com")
-
-    assert_response :success
-    expect(response).to be_success
-    expect(link.url).to eq(params[:url])
-    expect(link.count).to eq(2)
-  end
 end
