8e547b238d175109d071b7d0766959859ba4ee07
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-05 18:14:07 -0600

    add CORS gem

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,12 @@ module HotReads
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin' => 'http://localhost:3000/',
-        'Access-Control-Request-Method' => %w{PATCH POST OPTIONS}.join(",")
-      }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:patch, :post, :options]
+      end
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
